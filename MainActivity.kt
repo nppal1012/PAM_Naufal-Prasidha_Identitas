8b68977331dd1bdcb53219a6a3ed065137640866
nppal1012 <167274654+nppal1012@users.noreply.github.com>
2025-08-22 23:23:23 +0700

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.porto_pam
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.porto_pam.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.textView.text = "Agam Dwiyana"
+        }
+    }
+}
